116209061f937b7a30a1d63267e66f511b072dee
Simeon Tran <siatran@ucsc.edu>
2024-06-11 00:47:01 -0700

    Remove base string

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,6 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   site: "https://simeonat.github.io",
-  base: "",
   integrations: [tailwind(), sitemap(), mdx(), pagefind(),
                  react({experimentalReactChildren: true})],
   markdown: {
