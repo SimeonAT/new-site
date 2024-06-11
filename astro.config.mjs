7e79cdbcb18bfd641c8c3c573d8a7801364a3098
Simeon Tran <siatran@ucsc.edu>
2024-06-11 00:28:05 -0700

    Root base

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,7 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   site: "https://simeonat.github.io",
+  base: "",
   integrations: [tailwind(), sitemap(), mdx(), pagefind(),
                  react({experimentalReactChildren: true})],
   markdown: {
